d9139c27ac4f58518110db9f2fc1840bc0bee10d
Rizky I. F <rykzius@gmail.com>
2022-01-09 22:05:39 +0700

    [plugin] Version 1.0.2

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const CODENAME = "rykz-spcm"; // AUTOFILLED
 export const NAME = "rykz's Skyrim Platform Configuration Menu"; // AUTOFILLED
-export const VERSION = "1.0.1"; // AUTOFILLED
+export const VERSION = "1.0.2"; // AUTOFILLED
 
 export const SOUND_OPEN = 0x57F94;
 export const SOUND_CLOSE = 0x137E7;
